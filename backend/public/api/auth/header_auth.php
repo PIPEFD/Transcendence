ae22822929175b9010eb79e9257b6b2488d32343
Daniel Gonzalez Torralba <dangonz3@c3r1s1.42urduliz.com>
2025-09-24 16:03:06 +0200

    24 sep 2.0 1. Implementado logout: comprueba que el método sea el correcto y envía un mensaje confirmando el logout, el frontend debe eliminar el JWT

--- backend/public/api/auth/header_auth.php
+++ backend/public/api/auth/header_auth.php
@@ -4,8 +4,8 @@ header('Content-Type: application/json'); 	// Indica al navegador/cliente que la
 
 ini_set('display_errors', 1);				// Activa mostrar errores en pantalla para este proceso PHP (útil en desarrollo).
 ini_set('display_startup_errors', 1);		// Muestra errores que ocurren al arrancar PHP o extensiones antes de ejecutar el script.
-error_reporting(E_ALL);	
-// Pide a PHP que notifique todos los tipos de errores y avisos.
+error_reporting(E_ALL);						// Pide a PHP que notifique todos los tipos de errores y avisos.	 
+
 require_once __DIR__ . '../../../config/config.php';	// Carga el archivo que tiene la función de conexión a la base de datos y la creación de tablas.
 // __DIR__ => es una constante mágica, directorio del archivo actual, el interpréte de PHP la rellena en tiempo de compilación => 
 // PHP no interpreta rutas relativas “desde donde está el archivo” sino “desde el directorio de trabajo actual del proceso que ejecuta PHP”. Ese working directory no es fijo ni garantizado.
