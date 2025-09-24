22170fd8f100cab686c0a1595764930022d606da
Daniel Gonzalez Torralba <dangonz3@c3r1s1.42urduliz.com>
2025-09-24 17:15:30 +0200

    Cambio de rama

--- backend/public/api/auth/header_auth.php
+++ backend/public/api/auth/header_auth.php
@@ -20,4 +20,45 @@ function errorSend(int $errorCode, string $errorMsg, ?string $detailsMsg = null)
 	exit;
 }
 
+
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: GET, POST, DELETE, PATCH');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+header('Content-Type: application/json'); 	// Indica al navegador/cliente que la respuesta será texto en formato JSON.
+
+ini_set('display_errors', 1);				// Activa mostrar errores en pantalla para este proceso PHP (útil en desarrollo).
+ini_set('display_startup_errors', 1);		// Muestra errores que ocurren al arrancar PHP o extensiones antes de ejecutar el script.
+error_reporting(E_ALL);
+
+require 'utils.php';
+require '../config/config.php';
+$database = connectDatabase();
+$authToken = $_SERVER['HTTP_AUTHORIZATION'] ?? null;
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+$auth = checkAuthorization($authToken);
+$idFromAuth = extractIdFromAuth($authToken, $auth);
+$bodyArray = json_decode(file_get_contents('php://input'), true);
+$queryId = $_GET['id'] ?? null;
+
+$context = [
+    'database' => $database,
+    'requestMethod' => $requestMethod,
+    'auth' => $auth,
+    'tokenId' => $idFromAuth,
+    'queryId' => $queryId,
+    'body' => $bodyArray
+];
+
+?>
+
+
+
+
+
+
+
+
 ?>
