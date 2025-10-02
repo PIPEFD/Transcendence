7c19fc8cec9060c3469ea9a211e5548f6187eaee
Vera Garcia Diez <veragarc@c3r4s5.42urduliz.com>
2025-10-02 18:38:06 +0200

    sql problem

---
+++ backend/public/api/login.php
@@ -0,0 +1,65 @@
+<?php
+
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *'); // permite cualquier origen
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
+require_once __DIR__ . '/header.php';
+require_once __DIR__ . '/gmail_api/mail_gmail.php';
+
+$database = connectDatabase();
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+$body = json_decode(file_get_contents('php://input'), true);
+
+if ($requestMethod !== 'POST')
+	errorSend(405, 'unauthorized method');
+if (!stripos($_SERVER['CONTENT_TYPE'] ?? '', 'application/json')) // stripos() => STRing Insensitive POSition => Devuelve la posición de una subcadena en otra, sin distinguir mayúsculas/minúsculas. Si no encuentra la subcadena devuelve 'false'.
+	errorSend(415, 'unsupported media type');
+if (!is_array($body)) // El cuerpo del HTTP request debería ser JSON que represente un objeto. En PHP eso se traduce en un array asociativo. Si no lo es, el JSON es inválido o no tiene la estructura esperada.
+	errorSend(400, 'invalid json');
+if (!checkBodyData($body, 'username', 'password'))
+	errorSend(400, 'Bad request. Missing fields');
+
+$username = $body['username'];
+$passwordSent = $body['password'];
+
+$sqlQuery = "SELECT id, password, email FROM users WHERE username = :username";
+$bind1 = [':username', $username, SQLITE3_TEXT];
+$res1 = doQuery($database, $sqlQuery, $bind1);
+if (!$res1)
+	errorSend(500, "SQLite Error: " . $database->lastErrorMsg());
+$row = $res1->fetchArray(SQLITE3_ASSOC); // Para obtener filas concretas necesitas llamar a fetchArray() sobre $result. $row = $result->fetchArray(SQLITE3_ASSOC); SQLITE3_ASSOC => Indica que queremos la fila como array asociativo.
+if (!$row)
+	errorSend(404, 'username not found');
+
+$user_id = $row['id'];
+$passwordStored = $row['password'];
+$email = $row['email'];
+
+if (!password_verify($passwordSent, $passwordStored)) // $passwordStored contiene el hash + los medios para desencriptarlo
+	errorSend(401, 'invalid credentials');
+
+$stmt_delete = $database->prepare('DELETE FROM twofa_codes WHERE user_id = :user_id'); // Eliminamos cualquier entrada previa en la tabla twofa_codes para este usuario
+$stmt_delete->bindValue(':user_id', $user_id, SQLITE3_INTEGER);
+if (!$res_delete = $stmt_delete->execute()) // no da error porque no exista info previa sobre el user
+	errorSend(500, "SQLite Error: " . $database->lastErrorMsg());
+
+$two_fa_code = str_pad(random_int(0, 999999), 6, '0', STR_PAD_LEFT); // Generamos un código númerico aleatorio del 0 al 999999 (rellenamos con 0s empezando por la izq hasta tener 6 cifras)
+
+$stmt_insert = $database->prepare('INSERT OR REPLACE INTO twofa_codes (user_id, code) VALUES (:user_id, :code)'); // replace hace un DELETE seguido de un INSERT en la fila que causa el conflicto de la clave única
+$stmt_insert->bindValue(':user_id', $user_id, SQLITE3_INTEGER);
+$stmt_insert->bindValue(':code', $two_fa_code, SQLITE3_TEXT);
+if ($stmt_insert->execute() === false)
+	errorSend(500, 'couldn`t insert two_fa_code');
+
+if (!sendMailGmailAPI($user_id, $email, $two_fa_code))
+	errorSend(500, 'couldn\'t send mail with Gmail API');
+
+echo json_encode(['pending_2fa' => true, 'user_id' => $user_id]);
+exit;
--- backend/public/api/users.php
+++ backend/public/api/users.php
@@ -1,127 +1,154 @@
 <?php
 
-require_once '../utils/init.php';
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-// var_dump($context);
-$requestMethod = $context['requestMethod'];
-$queryId = $context['queryId'];
 
-switch ($requestMethod) {
-    case 'POST':
-        createUser($context); // no pide auth
-    case 'GET':
-        if ($queryId) {
-            userDataById($context); // no pide auth
-        }
-        else {
-            userList($context); // no pide auth
-        }
-    case 'PATCH':
-        editUserData($context); // pide auth
-    case 'DELETE':
-        deleteUser($context); // pide auth
-    default:
-        response(405, 'unauthorized method');
+require_once __DIR__ . '/header.php';
+
+$database = connectDatabase();
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+$body = json_decode(file_get_contents('php://input'), true);
+$queryId = $_GET['id'] ?? null;
+
+switch ($requestMethod)
+{
+	case 'POST':
+		createUser($body, $database);
+		break;
+	case 'GET':
+		if ($queryId)
+			userDataById($database, $queryId);
+		else
+			userList($database);
+		break;
+	case 'PATCH':
+		if (!checkJWT($queryId))
+			errorSend(403, 'forbidden access');
+		editUserData($queryId, $body, $database);
+		break;
+	case 'DELETE':
+		if (!checkJWT($queryId))
+			errorSend(403, 'forbidden access');
+		deleteUser($queryId, $database);
+		break;
+	default:
+		errorSend(405, 'unauthorized method');
 }
 
-function userList($context) {
-    $database = $context['database'];
+function createUser(array $body, SQLite3 $database): void
+{
+	if (!checkBodyData($body, 'username', 'email', 'password'))
+		errorSend(400, 'bad request');
+	$username = $body['username'];
+	$email = $body['email'];
+	$password = $body['password'];
 
-    $sqlQuery = "SELECT id, username, elo FROM users";
-    $res = $database->query($sqlQuery);
-    if (!$res)
-        response(500, 'Sql error: ' . $database->lastErrorMsg());
+	$passwordHash = password_hash($password, PASSWORD_DEFAULT);
 
-    $data = [];
-    while ($row = $res->fetchArray(SQLITE3_ASSOC))
-        $data[] = $row;
-
-    echo json_encode($data);
-    exit ;
+	$sqlQuery = "INSERT INTO users (username, email, password) VALUES (:username, :email, :password)";
+	$bind1 = [':username', $username, SQLITE3_TEXT];
+	$bind2 = [':email', $email, SQLITE3_TEXT];
+	$bind3 = [':password', $passwordHash, SQLITE3_TEXT];
+	$res = doQuery($database, $sqlQuery, $bind1, $bind2, $bind3);
+	if (!$res)
+		errorSend(500, 'Sql error: ' . $database->lastErrorMsg());
+	else
+		successSend('Created', 201, 'new UserID: ' . $database->lastInsertRowID());
 }
 
-function userDataById($context) {
-    $database = $context['database'];
-    $queryId = isId($context['queryId']);
-
-    $sqlQuery = "SELECT username, email, elo FROM users WHERE id = '$queryId'";
-    $res = $database->query($sqlQuery);
-    if (!$res)
-        response(500, 'Sql error: ' . $database->lastErrorMsg());
-    if (!($res->fetchArray(SQLITE3_ASSOC)))
-        response(404, 'user not found');
-
-    echo json_encode($res->fetchArray(SQLITE3_ASSOC));
-    exit ;
+function userDataById(SQLite3 $database, int $queryId): void
+{
+	$sqlQuery = "SELECT username, email, elo FROM users WHERE id = :queryId";
+	$bind1 = [':queryId', $queryId, SQLITE3_INTEGER];
+	$res = doQuery($database, $sqlQuery, $bind1);
+	if (!$res)
+		errorSend(500, 'Sql error: ' . $database->lastErrorMsg());
+	$userData = $res->fetchArray(SQLITE3_ASSOC); 
+	if (!$userData)
+		errorSend(404, 'user not found');
+	else
+		successSend($userData);
 }
 
-function createUser($context) {
-    $database = $context['database'];
-    $body = $context['body'];
-    
-    $username = getAndCheck($body, 'username');
-    $email = getAndCheck($body, 'email');
-    //var_dump($email);
-    $pass = getAndCheck($body, 'password');
-    $passwordHash = password_hash($pass, PASSWORD_DEFAULT);
-    $sqlQuery = "INSERT INTO users (username, email, pass) VALUES ('$username', '$email', '$passwordHash')";
-    $res = $database->exec($sqlQuery);
-    if (!$res) {
-        response(500, 'Sql error: ' . $database->lastErrorMsg());
-    }
-
-    echo json_encode(['success' => 'new user created']);
-    exit ;
+function userList(SQLite3 $database): void
+{
+	$res = doQuery($database, "SELECT id, username, elo FROM users");
+	if (!$res)
+		errorSend(500, 'Sql error: ' . $database->lastErrorMsg());
+	$data = [];
+	while ($row = $res->fetchArray(SQLITE3_ASSOC)) // fetchArray tiene un indice interno que aumenta con cada llamada
+		$data[] = $row;
+	successSend($data);
 }
 
-function editUserData($context) {
-    $id = $context['tokenId'];
-    if ($id !== $context['queryId'])
-        response(403, 'forbidden access');
-    $body = $context['body'];
-    $database = $context['database'];
-
-    if ($context['body']['password'])
-        editUserPass($id, $body, $database);
-
-    $username = getAndCheck($body, 'username');
-    $email = getAndCheck($body, 'email');
-
-    $updates = [ "username = '$username'", "email = '$email'" ];
-    $sqlQuery = "UPDATE users SET" . implode(', ', $updates) . "WHERE id = '$id'";
-    $res = $database->exec($sqlQuery);
-    if (!res)
-        response(500, 'Sql error: ' . $database->lastErrorMsg());
-
-    echo json_encode(['success' => 'user data modified']);
-    exit ;
+function editUserData(int $queryId, array $body, SQLite3 $database): void
+{
+	$database->exec('BEGIN'); // inicia una transacción. Declara un paquete de operaciones SQL, si alguna de ellas falla revierte el paquete completo.
+	try
+	{
+		$success = true;
+		if (checkBodyData($body, 'password'))
+		{
+			$newPassword = $body['password'];
+			$newPasswordHash = password_hash($newPassword, PASSWORD_DEFAULT);
+			if (!editUserDataAux($queryId, 'password', $newPasswordHash, $database))
+				$success = false;
+		}
+		if (checkBodydata($body, 'username'))
+			if (!editUserDataAux($queryId, 'username', $body['username'], $database))
+				$success = false;
+		if (checkBodydata($body, 'email'))
+			if (!editUserDataAux($queryId, 'email', $body['email'], $database))
+				$success = false;
+		if ($success)
+		{
+			$database->exec('COMMIT');
+			successSend('user data modified');
+		}
+		else
+			throw new Exception('One of the update operations failed');
+	}
+	catch (Exception $e)
+	{
+		$database->exec('ROLLBACK');
+		errorSend(500, 'Could not update user data', $e->getMessage());
+	}
 }
 
-function editUserPass($id, $body, $database) {
-    $newPassword = getAndCheck($body, 'password');
-    $newPasswordHash = password_hash($newPassword, PASSWORD_DEFAULT);
-
-    $sqlQuery = "UPDATE users SET pass = '$newPasswordHash' WHERE id = '$id'";
-    $res = $database->exec($sqlQuery);
-    if (!res)
-        response(500, 'Sql error: ' . $database->lastErrorMsg());
-
-    echo json_encode(['success' => 'password updated']);
-    exit ;
+function editUserDataAux(int $queryId, string $column, string $newValue, SQLite3 $database): bool
+{
+	switch ($column) 
+	{ // solo podemos insertar con prepare() valores, ni nombres de tablas ni columnas
+		case 'password': 
+			$sqlQuery = "UPDATE users SET password = :newValue WHERE id = :queryId"; 
+			break;
+		case 'username':
+			$sqlQuery = "UPDATE users SET username = :newValue WHERE id = :queryId";
+			break;
+		case 'email':
+			$sqlQuery = "UPDATE users SET email = :newValue WHERE id = :queryId";
+			break;
+	}
+	$bind1 = [':newValue', $newValue, SQLITE3_TEXT];
+	$bind2 = [':queryId', $queryId, SQLITE3_INTEGER];
+	$res = doQuery($database, $sqlQuery, $bind1, $bind2);
+	if(!$res)
+		return false;
+	else
+		return true;
 }
 
-function deleteUser($context) {
-    if ($context['tokenId'] !== $context['queryId'])
-        response(403, 'forbidden access');
-    $database = $context['database'];
-
-    $sqlQuery = "DELETE FROM users WHERE id = :id";
-    $res = $database->exec($sqlQuery);
-    if (!$res)
-        response(500, 'Sql error: ' . $database->lastErrorMsg());
-    
-    echo json_encode(['success' => 'user deleted']);
-    exit ;
+function deleteUser(int $queryId, SQLite3 $database) 
+{
+	$sqlQuery = "DELETE FROM users WHERE id = :queryId";
+	$bind1 = [':queryId', $queryId, SQLITE3_INTEGER];
+	$res = doQuery($database, $sqlQuery, $bind1);
+	if (!$res) // los execute() de UPDATE y DELETE no devuelven lineas -> fetchArray() no es necesario
+		errorSend(500, 'Sql error: ' . $database->lastErrorMsg());
+	else
+		successSend('user deleted'); // no se pueden imprimir varias cadenas JSON -> solo la función principal echo-ea
 }
 
 ?>
